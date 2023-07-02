59188575b0e46d01199ec09bc484e6a84f2e3a74
BryanOR96 <113558432+BryanOR96@users.noreply.github.com>
2023-07-02 11:20:35 -0600

    Conexion

---
+++ LENGUAJEDB/PROYECTO_LENGUAJEDB/src/main/java/Conexiones/ConexionOracle.java
@@ -0,0 +1,42 @@
+package Conexiones;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class ConexionOracle {
+
+    private final String DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private final String URL = "jdbc:oracle:thin:@//localhost:1521/xe";
+    private final String USER = "DBSISTEMA";
+    private final String PASWORD = "Borrajasa1212";
+
+    public Connection cadena;
+
+    public ConexionOracle() {
+         this.cadena = null;
+    }
+
+    public Connection conectar() {
+        try {
+            Class.forName(DRIVER);
+            this.cadena = DriverManager.getConnection(URL, USER, PASWORD);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            System.exit(0);
+        }
+        return this.cadena;
+
+    }
+
+    public void desconectar() {
+        try {
+            this.cadena.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+
+}
--- LENGUAJEDB/PROYECTO_LENGUAJEDB/src/main/java/com/mycompany/proyecto_lenguajedb/PROYECTO_LENGUAJEDB.java
+++ LENGUAJEDB/PROYECTO_LENGUAJEDB/src/main/java/com/mycompany/proyecto_lenguajedb/PROYECTO_LENGUAJEDB.java
@@ -2,10 +2,33 @@
 
 package com.mycompany.proyecto_lenguajedb;
 
+import Conexiones.ConexionOracle;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PROYECTO_LENGUAJEDB {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String sSQL ="";
+        ConexionOracle conexion = new ConexionOracle();
+        
+        try{
+            sSQL = "SELECT * FROM PROVEEDOR";
+           Connection con = conexion.conectar();
+           Statement cn = con.createStatement();
+           ResultSet res = cn.executeQuery(sSQL);
+           
+           while (res.next()) {
+               System.out.println("-----------------");
+               System.out.println(res.getInt("ID_Vendedor"));
+               System.out.println(res.getString("Nombre"));
+               System.out.println(res.getString("telefono"));
+               System.out.println(res.getString("direccion"));
+           }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
     }
 }
