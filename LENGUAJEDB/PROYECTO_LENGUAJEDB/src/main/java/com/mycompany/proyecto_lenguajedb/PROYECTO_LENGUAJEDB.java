6a0bff4908d69dc0e15040be3d7a792fc1dcb13b
BryanOR96 <113558432+BryanOR96@users.noreply.github.com>
2023-07-01 21:26:18 -0600

    Semana7
    Creacion de base de datos, cruds, procesos almcenados y coneccion de java a DB oracle

---
+++ LENGUAJEDB/PROYECTO_LENGUAJEDB/src/main/java/com/mycompany/proyecto_lenguajedb/PROYECTO_LENGUAJEDB.java
@@ -0,0 +1,11 @@
+
+
+package com.mycompany.proyecto_lenguajedb;
+
+
+public class PROYECTO_LENGUAJEDB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
